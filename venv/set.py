728809e26c15884beb2754dc29aa2a0a8897915a
umavallu <69756854+umavallu@users.noreply.github.com>
2020-08-17 07:00:56 -0700

    List element and its count

--- venv/set.py
+++ venv/set.py
@@ -1,3 +1,4 @@
+'''
 s={1,2,3,4}
 s1=s.copy()
 s.add(5)
@@ -22,5 +23,28 @@ print(s4)
 s3={1,2,3,4,5,6,7,2,3,4,5,6,7,8,9,1,2,3,4,5,6,9,8,10,30}
 s4=s3
 print(s4)
+'''
+s={1,2,3,'hi',4.5,(6,7,8)}
+for i in range(len(s)):
+    if len(s) != 0:
+        s.pop()
+        print(s)
 
+'''
+s={1,2,3,4,'hi',(1,2,34,7)}
+#Removing all the elements from set
+#method1 using pop()
+l=s.copy()
+for i in l:
+    if i in s:
+        s.pop()
+print(s)
 
+#method2 using remove(i)
+s={1,2,3,4,'hi'}
+l=s.copy()
+for i in l:
+    if i in s:
+        s.remove(i)
+print(s)
+'''
