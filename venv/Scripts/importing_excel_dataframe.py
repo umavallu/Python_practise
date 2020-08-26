537d6f25678838a5f8d75089f89267d16ee21516
umavallu <69756854+umavallu@users.noreply.github.com>
2020-08-26 06:24:48 -0700

    filter() and map() with/with out lambda

--- venv/Scripts/importing_excel_dataframe.py
+++ venv/Scripts/importing_excel_dataframe.py
@@ -1,107 +1,49 @@
+#working with map() and lambda() to assign each list value with doubled value
+def twice(num):
+    double=num*2
+    return double
+l=[1,2,3,4,5,6,7,8,9,10,11]
+l2=list(map(twice,l))
+print('2 table for the given list:',l2)
+#3table
+l3=list(map(lambda x:x*3,l))
+print('3 table for the given list is:',l3)
+#Lambda Functions
+#to find squares
+s= (lambda x:x*x)
+print('Square of {} is {} ',4,s(4))
+print('Square of {} is {} ',10,s(10))
+#to find sum of two numbers
+sum=(lambda a,b:a+b)
+print('Addition of 7 and 8 is:',sum(7,8))
+print('Addition of 29091987 and 3 is:',sum(29091987,3))
+#to find whether a number is even or odd
+even_odd= (lambda x:'Even' if x%2==0  else 'Odd')
+print('Is 8 even/odd: ',even_odd(8))
+print('Is 59 even/odd: ',even_odd(59))
 
-a=10
-def f1():
-    global a
-    print(a)
 
-f1()
-#positional arguments
-def multiplication_table(a):
-  for i in range(1,21):
-      print('{} * {} = {}'.format(a,i,a*i))
-multiplication_table(1) # 1 is the positional argument
-multiplication_table(2) # 2 is the positional argument
-multiplication_table(3) # 3 is the positional argument
-# Finding Factorial
-#Method1
-def factorial(a):
-    fact_value=a
-    i=a
-    while i>1:
-        fact_value=fact_value*(i-1)
-        i=i-1
-    return fact_value
-print('Factorial value of 3 is:',factorial(3))
-#Method2
-def factorial(a):
-    fact_value=a
-    if a !=1:
-        fact_value=fact_value*factorial(a-1)
-    return fact_value
-print('factorial of 4 is :',factorial(4))
-#Returning multiple values
-def arithmetic_op(a,b): #def arithmetic_op(a,b=1)--> no error and def arithmetic_op(a=10,b)---> error
-    add_value=a+b
-    diff_value=(a-b if a>b else b-a)
-    mul_value=a*b
-    div_value=a/b
-    exp_value=a**b
-    return add_value,diff_value,mul_value,div_value,exp_value
-a,b,c,d,e=arithmetic_op(10,4) #a,b,c,d,e=arithmetic_op(10,b=4)=> no error and a,b,c,d,e=arithmetic_op(a=10,4)-->error
-print('Addition of 10 and 4 is:',a)
-print('Difference of 10 and 4 is:',b)
-print('10*4 is:',c)
-print('10/4 is:',d)
-print('exponential value of 10 and 4 is:',e)
-
-#default arguments
-def sum(a,b=5):
-    sum=a+b
-    print('Total of {} and {} is: {}'.format(a,b,sum))
-sum(a=6,6)
-
-#variable length arguments
-#variable length argument with *
-def tuple_t(*args,n):
-    sum,total=0,0
-    for i in args:
-        sum=sum+i
-    total=sum+n
-    return total,sum
-total,sum=tuple_t(10,20,30,40,n=100)
-print('total: ',total,' sum: ',sum)
-#variable length arguments with * and **
-def tuple_dict(*marks,**stu_details):
-    score=0
-    for i in marks:
-        score=score+i
-    j=stu_details.items()
-    return score,j
-d={}
-total,d=tuple_dict(10,20,30,40,50,name='uma',school='saikrishna',class_name='board')
-for k,v in d:
-    print(k,' : ',v)
-print('score: ',total)
-
-######################################
-# 0 1 1 2 3 5 8
-def fib(num):
-    if num == 0:
-        return 0
-    elif num == 1:
-        return 1
+#working with filter() and lambda()
+#Here iseven is taking each element in list l as an argument and return True/False
+#Filter function returns each list value which is True
+def iseven(num):
+    if num%2==0:
+        return True
     else:
-        return (fib(num - 2) + fib(num - 1))
+        return False
+l=[int(i) for i in input('enter values for list: ').split(' ')]
+print(l)
+l1=list(filter(iseven,l))
+print('Even list of {} is :{}'.format(l,l1))
+l2= list(filter((lambda x: True if x%2!=0 else False),l))
+print('Odd list of {} is :{}'.format(l,l2))
 
-
-num = int(input('Enter how many numbers u want in fibonacci series: '))
-for i in range(0, num):
-    print(fib(i), end=' ')
-
-
-#Function with unknown number of arguments
-def tuple_sum(*l):
-    sum=0
-    for i in l:
-        sum=sum+i
-    return sum
-print('tuple_sum([1,2,3,4,5] : ',tuple_sum(1,2,3,4,5))
-#Function to add elements in list(created using list comprehension)
-j=[int(i) for i in input('enter list:').split(',')]
-print(j)
-def list_collection_sum(l):
-    sum=0
-    for i in l:
-        sum=sum+i
-    return sum
-print('sum of {} is : {}'.format(j,list_collection_sum(j)))
+#Accessing global values globals()['variablename']
+a=10
+def f1(x):
+    a=20
+    print('Product of {} and {} is: {} '.format(a,x,x*(globals()['a'])))
+def f2(x):
+    print('Addition of {} and {} is: {}'.format(a,x,x+a))
+f1(2)
+f2(2)
