cc8a3735da510699a3793a3432aaeb077d4a207a
umavallu <69756854+umavallu@users.noreply.github.com>
2020-09-07 06:02:38 -0700

    FILE I/O

--- venv/Scripts/importing_excel_dataframe.py
+++ venv/Scripts/importing_excel_dataframe.py
@@ -1,49 +1,14 @@
-#working with map() and lambda() to assign each list value with doubled value
-def twice(num):
-    double=num*2
-    return double
-l=[1,2,3,4,5,6,7,8,9,10,11]
-l2=list(map(twice,l))
-print('2 table for the given list:',l2)
-#3table
-l3=list(map(lambda x:x*3,l))
-print('3 table for the given list is:',l3)
-#Lambda Functions
-#to find squares
-s= (lambda x:x*x)
-print('Square of {} is {} ',4,s(4))
-print('Square of {} is {} ',10,s(10))
-#to find sum of two numbers
-sum=(lambda a,b:a+b)
-print('Addition of 7 and 8 is:',sum(7,8))
-print('Addition of 29091987 and 3 is:',sum(29091987,3))
-#to find whether a number is even or odd
-even_odd= (lambda x:'Even' if x%2==0  else 'Odd')
-print('Is 8 even/odd: ',even_odd(8))
-print('Is 59 even/odd: ',even_odd(59))
-
-
-#working with filter() and lambda()
-#Here iseven is taking each element in list l as an argument and return True/False
-#Filter function returns each list value which is True
-def iseven(num):
-    if num%2==0:
-        return True
-    else:
-        return False
-l=[int(i) for i in input('enter values for list: ').split(' ')]
-print(l)
-l1=list(filter(iseven,l))
-print('Even list of {} is :{}'.format(l,l1))
-l2= list(filter((lambda x: True if x%2!=0 else False),l))
-print('Odd list of {} is :{}'.format(l,l2))
-
-#Accessing global values globals()['variablename']
-a=10
-def f1(x):
-    a=20
-    print('Product of {} and {} is: {} '.format(a,x,x*(globals()['a'])))
-def f2(x):
-    print('Addition of {} and {} is: {}'.format(a,x,x+a))
-f1(2)
-f2(2)
+from sample_before_commit import *
+f=input('Enter file name to append data: ')
+if f=='fileio.txt':
+    file_write(f)
+elif f=='namesio.txt':
+    l=[]
+    l=name_print(f)
+    print('values of file')
+    print(l)
+else:
+    print('File does not exists')
+b=open(f,'r')
+data=b.read()
+print(data)
--- venv/Scripts/sample_before_commit.py
+++ venv/Scripts/sample_before_commit.py
@@ -1,60 +1,16 @@
-#USER DEFINED MODULE
-#This module is saved in the file detaila_module.py
-name='uma'
-husband='vijay'
-child1,child2='adithya','pradyumna'
-def parents(name):
-    if name.upper()=='UMA':
-            parents='Koteswararao and Ramadevi'
-    else:
-            parents='This persons parents records are not available'
-    return parents
-def printing(name):
-    if name.upper()=='UMA':
-        print('printing your name:', name)
-    return name
-''''Using the above user defined module in another python file'''
-#All the below python files use the above module in different scenarios
-
-#METHOD1
-from details_module import name,husband,child1,child2
-def print_details():
-    print('Your name is:',name)
-    print('Your name is:',husband)
-    print('Your name is:',child1)
-    print('Your name is:',child2)
-    #print('Your name is:',parents)
-print_details()
-#METHOD2
-from details_module import *
-def print_details():
-    print('Your name is:',name)
-    print('Your name is:',husband)
-    print('Your name is:',child1)
-    print('Your name is:',child2)
-    #print('Your name is:',parents)
-print_details()
-#method3
-import details_module as dm
-def print_details():
-    print('Your name is:',dm.name)
-    print('Your name is:',dm.husband)
-    print('Your name is:',dm.child1)
-    print('Your name is:',dm.child2)
-    print('Your name is:',parents)
-print_details()
-#method4 sleep(15) and reload()
-
-import details_module as dm
-import time
-def print_details():
-    print('Your name is:',dm.name)
-    print('Your name is:',dm.husband)
-    print('Your name is:',dm.child1)
-    print('Your name is:',dm.child2)
-    print('Your parents are:',dm.parents('uma'))
-    print('Program is entering into sleeping state')
-    time.sleep(45)
-    dm.printing('uma')
-    print('Program is waking up')
-print_details()
+def file_write(file_name):
+    f=open(file_name,'a+')
+    l=input('enter what you want to write into the file: ').split(" ")
+    for i in l:
+        f.writelines(i+' ')
+def name_print(file_name):
+    f=open(file_name,'r')
+    l=[]
+    i=f.readline()
+    while i!=' ':
+        if i==' ':
+            break
+        else:
+            l.append(i)
+            i=f.readline()
+    return l
